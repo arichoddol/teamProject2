0c8b78150dcec038403c8afe304c304e21c29fc3
wjdrlgh93 <mario115@naver.com>
2025-12-01 10:20:02 +0900

    stop Tracking Gitginore commonApis

--- vite-front/src/apis/commonApis.jsx
+++
@@ -1,3 +0,0 @@
-export const FRONT_BASIC_URL = `http://localhost:3000`;
-export const BACK_BASIC_URL = `http://localhost:8088`;
-export const IMAGES_S3_URL = `https://spring-project-test.s3.ap-northeast-2.amazonaws.com/`;
